f58be5285cb33d6f9f313e125ed59447da60a9d2
RafaelRodrigues-mg <64231226+RafaelRodrigues-mg@users.noreply.github.com>
2020-04-24 15:44:17 -0300

    faceboom-rr

--- faceboom-rr.py
+++ faceboom-rr.py
@@ -13,6 +13,11 @@ def main():
         user = input('USERNAME FACEBOOK: ')
         if(min.isnumeric() == True and max.isnumeric() == True and opt != '' and user != ''):
             os.system('clear')
+            vp = input('python3.X(Digite o valor de X ou use ENTER para deixar para versão python3): ')
+            if(vp.isnumeric() == True):
+                python = 'python3.'+vp
+            else:
+                python = 'python3'
             print('EXECUTANDO CRUNCH')
             sleep(1)
             print('3...')
@@ -21,7 +26,7 @@ def main():
             sleep(1)
             print('1...')
             sleep(1)
-            os.system('crunch '+min+' '+max+' '+opt+' | python3.7 fac.py '+user)
+            os.system('crunch '+min+' '+max+' '+opt+' | '+python+' fac.py '+user)
 try:
     main()
 except KeyboardInterrupt:
